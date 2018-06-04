5a7ba286b5e25f183d1bd1cacc3958d08f31db1d
Shubham <sjais789@users.noreply.github.com>
2018-06-04 21:21:21 +0200

    Update RandomForestRegressor.py

--- RandomForestRegressor.py
+++ RandomForestRegressor.py
@@ -14,13 +14,15 @@ c.score(boston['data'],boston['target'])
 #boston['data'].shape 
 
 
-"""row= boston ['data'][20]
+""" row= boston ['data'][20]
 row.shape
 
 
 q=row.reshape(-1,15)
 
 c.predict(q)
-boston['target'][20] """
+boston['target'][20]  """
 
+from sklearn.model_selection import train_test_split
 
+xtrain, ytrain , xtest,ytest=train_test_split(boston['data'],bsoton['target'],test_size(0.4)) 
